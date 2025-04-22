5cbd7a8f75e0afbfa69e176a2aff39580e23bf4a
Viktoriia Asliamova <asliamova.vikki@gmail.com  ci = commit>
2025-04-22 20:05:19 +0300

    feat: created new class Utils. And created method check sort

---
+++ src/main/java/org/opencart/utils/Utils.java
@@ -0,0 +1,16 @@
+package org.opencart.utils;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Utils {
+    public static <T> boolean isSortedAscending(List<T> list, Comparator<T> comparator) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (comparator.compare(list.get(i), list.get(i + 1)) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
