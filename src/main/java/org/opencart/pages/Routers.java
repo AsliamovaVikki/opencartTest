d3a3853ce707d4992563945e26f459951232f52c
Viktoriia Asliamova <asliamova.vikki@gmail.com  ci = commit>
2025-04-23 12:37:09 +0300

    build: created new cls with routers

---
+++ src/main/java/org/opencart/pages/Routers.java
@@ -0,0 +1,10 @@
+package org.opencart.pages;
+
+public class Routers {
+  public static final String BRANDS_URL = "route=product/manufacturer";
+  public static final String IPHONE_URL = "route=product/product&product_id=40";
+  public static final String CAMERAS_URL = "route=product/category&path=33";
+  public static final String SHOW_ALL_URL = "route=product/category&path=20";
+  public static final String REGISTER_URL = "route=account/register";
+  public static final String SUBMIT_REGISTER_URL = "route=account/success";
+}
