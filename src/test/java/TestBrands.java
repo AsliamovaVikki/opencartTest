54301f12e06e6de679124dea92de0590a24abd99
Viktoriia Asliamova <asliamova.vikki@gmail.com  ci = commit>
2025-04-28 19:47:52 +0300

    style: formatting code test: add Allure annotations

--- src/test/java/TestBrands.java
+++ src/test/java/TestBrands.java
@@ -1,26 +1,47 @@
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import io.qameta.allure.*;
+import io.qameta.allure.junit5.AllureJunit5;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.opencart.pages.PageBrands;
 
 import java.util.List;
-import org.junit.jupiter.api.Test;
-import org.opencart.pages.PageBrands;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Epic("Library")
+@Feature("Filter by Brands")
 
 public class TestBrands extends BaseTest<PageBrands> {
 
-  public TestBrands() {
-    this.classPage = PageBrands.class;
-  }
+	public TestBrands() {
+		this.classPage = PageBrands.class;
+	}
 
-  @Test
-  public void testBrandsAreDisplayed() {
-    List<String> expectedBrands =
-        List.of("Apple", "Canon", "Hewlett-Packard", "HTC", "Palm", "Sony");
 
-    page.openPage();
+	@Test
+	@Story("Checking brands")
+	@Severity(SeverityLevel.NORMAL)
+	@DisplayName("Check successfully display brands list on Brands page")
+	@Description("The test checks the successful opening of the Brands page and availability on the list of the required brands")
+	@ExtendWith(AllureJunit5.class)
+	@Tag("smoke")
+	public void testBrandsAreDisplayed() {
+		List<String> expectedBrands =
+				List.of("Apple", "Canon", "Hewlett-Packard", "HTC", "Palm", "Sony");
 
-    List<String> actualBrands = page.getBrandNames();
+		Allure.step("The Brands page opens");
+		page.openPage();
 
-    for (String brand : expectedBrands) {
-      assertTrue(actualBrands.contains(brand), "Brand not found: " + brand);
-    }
-  }
+
+		Allure.step("Check the availability of brands on the page");
+		List<String> actualBrands = page.getBrandNames();
+
+		for (String brand : expectedBrands) {
+			assertTrue(actualBrands.contains(brand), "Brand not found: " + brand);
+		}
+	}
+
+
 }
