9182047abf1504d962dd2acc9b828ca4488e1516
Viktoriia Asliamova <asliamova.vikki@gmail.com  ci = commit>
2025-04-28 19:44:55 +0300

    style: formatting code refactor: add some Allure annotation

--- src/main/java/org/opencart/pages/PageProducts.java
+++ src/main/java/org/opencart/pages/PageProducts.java
@@ -1,29 +1,32 @@
 package org.opencart.pages;
 
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import static org.opencart.locators.ProductsLocators.CAMERAS_BUTTON;
 import static org.opencart.locators.ProductsLocators.xPathProduct;
 import static org.opencart.locators.ShowAllDesktopsLocators.PRODUCTS;
 import static org.opencart.pages.Routers.CAMERAS_URL;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 public class PageProducts extends BasePage {
 
-  public PageProducts(WebDriver driver, WebDriverWait wait) {
-    super(driver, wait);
-  }
+	public PageProducts(WebDriver driver, WebDriverWait wait) {
+		super(driver, wait);
+	}
 
-  public void openPage() {
-    clickAndWaitForUrl(CAMERAS_BUTTON, CAMERAS_URL);
-  }
+	public void openPage() {
+		clickAndWaitForUrl(CAMERAS_BUTTON, CAMERAS_URL);
+	}
 
-  public boolean checkCountProducts(String value) {
-    return checkCount(PRODUCTS, value);
-  }
+	@Step("Check count products")
+	public boolean checkCountProducts(String value) {
+		return checkCount(PRODUCTS, value);
+	}
 
-  public boolean checkPriceProduct(String nameProduct, String methodPrice, String price) {
-    String priceProduct = findElement(xPathProduct(nameProduct, methodPrice)).getText();
-    return priceProduct.equals(price);
-  }
+	@Step("Check price product")
+	public boolean checkPriceProduct(String nameProduct, String methodPrice, String price) {
+		String priceProduct = findElement(xPathProduct(nameProduct, methodPrice)).getText();
+		return priceProduct.equals(price);
+	}
 }
