5d991227534a22d6b027539bf9dce3df4336f365
Viktoriia Asliamova <asliamova.vikki@gmail.com  ci = commit>
2025-04-28 19:45:21 +0300

    style: formatting code refactor: add some Allure annotation

--- src/main/java/org/opencart/pages/PageShowAllDesktops.java
+++ src/main/java/org/opencart/pages/PageShowAllDesktops.java
@@ -1,83 +1,96 @@
 package org.opencart.pages;
 
-import static org.opencart.locators.ShowAllDesktopsLocators.*;
-import static org.opencart.pages.Routers.SHOW_ALL_URL;
-import static org.opencart.scripts.JavaScripts.SCROLL_TO_DOWN_PAGE_JS;
-import static org.opencart.utils.Utils.isSortedAscending;
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.opencart.locators.ShowAllDesktopsLocators.*;
+import static org.opencart.pages.Routers.SHOW_ALL_URL;
+import static org.opencart.scripts.JavaScripts.SCROLL_TO_DOWN_PAGE_JS;
+import static org.opencart.utils.Utils.isSortedAscending;
 
 public class PageShowAllDesktops extends BasePage {
 
-  public PageShowAllDesktops(WebDriver driver, WebDriverWait wait) {
-    super(driver, wait);
-  }
+	public PageShowAllDesktops(WebDriver driver, WebDriverWait wait) {
+		super(driver, wait);
+	}
 
-  public void openPage() {
-    cursorGuidance(DESKTOPS);
-    clickAndWaitForUrl(SHOW_ALL, SHOW_ALL_URL);
-  }
+	public void openPage() {
+		cursorGuidance(DESKTOPS);
+		clickAndWaitForUrl(SHOW_ALL, SHOW_ALL_URL);
+	}
 
-  public boolean checkInShow(String value) {
-    return dropdownSelectCheck(OPTION_SHOW, value);
-  }
+	@Step("Check in show value in the dropdown inShow")
+	public boolean checkInShow(String value) {
+		return dropdownSelectCheck(OPTION_SHOW, value);
+	}
 
-  public boolean checkInSortBy(String value) {
-    return dropdownSelectCheck(OPTION_SORT, value);
-  }
+	@Step("Check in sort value in the dropdown inSort")
+	public boolean checkInSortBy(String value) {
+		return dropdownSelectCheck(OPTION_SORT, value);
+	}
 
-  public boolean checkCountProducts(String value) {
-    return checkCount(PRODUCTS, value);
-  }
+	@Step("Check count products")
+	public boolean checkCountProducts(String value) {
+		return checkCount(PRODUCTS, value);
+	}
 
-  public void switchShowDD(String value) {
-    switchSelect(OPTION_SHOW, value);
-  }
+	@Step("Switch show dropdown")
+	public void switchShowDD(String value) {
+		switchSelect(OPTION_SHOW, value);
+	}
 
-  public void switchSortByDD(String value) {
-    switchSelect(OPTION_SORT, value);
-  }
+	@Step("Switch sortBy dropdown")
+	public void switchSortByDD(String value) {
+		switchSelect(OPTION_SORT, value);
+	}
 
-  public WebElement getPagination() {
-    return findElement(PAGINATION);
-  }
+	@Step("Get pagination")
+	public WebElement getPagination() {
+		return findElement(PAGINATION);
+	}
 
-  public boolean checkPaginationText(String value) {
-    String textPagination = getPagination().getText();
-    return textPagination.equals(value);
-  }
+	@Step("Check pagination text")
+	public boolean checkPaginationText(String value) {
+		String textPagination = getPagination().getText();
+		return textPagination.equals(value);
+	}
 
-  public void scrollPage() {
-    executeScript(SCROLL_TO_DOWN_PAGE_JS);
-  }
+	@Step("Scroll page")
+	public void scrollPage() {
+		executeScript(SCROLL_TO_DOWN_PAGE_JS);
+	}
 
-  public boolean checkPaginationLocate() {
-    WebElement pagination = getPagination();
-    return pagination.isDisplayed();
-  }
+	@Step("Check pagination locate")
+	public boolean checkPaginationLocate() {
+		WebElement pagination = getPagination();
+		return pagination.isDisplayed();
+	}
 
-  public boolean checkAlphabetOrder() {
-    List<WebElement> products = findElements(PRODUCTS_NAMES);
-    List<String> productNames =
-        products.stream().map(e -> e.getText().trim()).collect(Collectors.toList());
-    return isSortedAscending(productNames, String.CASE_INSENSITIVE_ORDER);
-  }
+	@Step("Check alphabet order")
+	public boolean checkAlphabetOrder() {
+		List<WebElement> products = findElements(PRODUCTS_NAMES);
+		List<String> productNames =
+				products.stream().map(e -> e.getText().trim()).collect(Collectors.toList());
+		return isSortedAscending(productNames, String.CASE_INSENSITIVE_ORDER);
+	}
 
-  public boolean checkPriceOrder() {
-    List<WebElement> products = findElements(PRODUCTS_PRICE);
-    List<Double> productsPrice = new ArrayList<>();
+	@Step("Check price order")
+	public boolean checkPriceOrder() {
+		List<WebElement> products = findElements(PRODUCTS_PRICE);
+		List<Double> productsPrice = new ArrayList<>();
 
-    for (WebElement el : products) {
-      String priceText = el.getText().replace("$", "").trim();
-      double price = Double.parseDouble(priceText);
-      productsPrice.add(price);
-    }
-    return isSortedAscending(productsPrice, Comparator.naturalOrder());
-  }
+		for (WebElement el : products) {
+			String priceText = el.getText().replace("$", "").trim();
+			double price = Double.parseDouble(priceText);
+			productsPrice.add(price);
+		}
+		return isSortedAscending(productsPrice, Comparator.naturalOrder());
+	}
 }
