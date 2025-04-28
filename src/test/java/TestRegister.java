51a33b6d2dbc18ee12a84e116d5549b631fb5035
Viktoriia Asliamova <asliamova.vikki@gmail.com  ci = commit>
2025-04-28 19:48:02 +0300

    style: formatting code test: add Allure annotations

--- src/test/java/TestRegister.java
+++ src/test/java/TestRegister.java
@@ -1,31 +1,43 @@
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import io.qameta.allure.*;
+import io.qameta.allure.junit5.AllureJunit5;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.opencart.pages.PageRegistration;
 import org.opencart.utils.RandomUser;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Epic("Авторизація/Автентифікація")
+@Feature("Реєстрація")
+@ExtendWith(AllureJunit5.class)
+
 public class TestRegister extends BaseTest<PageRegistration> {
-  private final RandomUser user = new RandomUser();
+	private final RandomUser user = new RandomUser();
 
-  public TestRegister() {
-    this.classPage = PageRegistration.class;
-  }
+	public TestRegister() {
+		this.classPage = PageRegistration.class;
+	}
 
-  @Test
-  public void testRegisterAccount() {
-    String expectedText = "Your Account Has Been Created!";
+	@Test
+	@Story("Successful user registration")
+	@Severity(SeverityLevel.CRITICAL)
+	@DisplayName("Checking successful registration and transition to user's personal account")
+	public void testRegisterAccount() {
+		String expectedText = "Your Account Has Been Created!";
+		Allure.step("Opening the site page");
+		page.openPage();
 
-    page.openPage();
-
-    page.fillRegisterForm(
-        user.getFirstName(),
-        user.getLastName(),
-        user.getEmail(),
-        user.getPhone(),
-        user.getPassword(),
-        user.getPassword());
-
-    page.confirmForm();
-    assertTrue(page.checkRegister(expectedText), "Register is successful");
-  }
+		Allure.step("Creating random user data");
+		page.fillRegisterForm(
+				user.getFirstName(),
+				user.getLastName(),
+				user.getEmail(),
+				user.getPhone(),
+				user.getPassword(),
+				user.getPassword());
+		Allure.step("Fill in all fields of registration form and transition to a personal page!");
+		page.confirmForm();
+		assertTrue(page.checkRegister(expectedText), "Register is successful");
+	}
 }
